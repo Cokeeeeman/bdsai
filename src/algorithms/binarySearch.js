6d1be8b66cf8132bfcf818e31d19d469100fda0a
Ziyue Chen <zchen@zobristinc.com>
2018-05-09 09:02:49 -0700

    Add binarySearch() and its tests

---
+++ src/algorithms/binarySearch.js
@@ -0,0 +1,27 @@
+/**
+ * Binary search
+ * @param  {Array} array  Input array
+ * @param  {Number} target Target value
+ * @return {Number}        The index of the target
+ */
+const binarySearch = (array, target) => {
+	if (!Array.isArray(array) || !target) {
+		return -1;
+	};
+
+	let l = 0, r = array.length - 1;
+	while (l <= r) {
+		let m = Math.ceil((l + r) / 2);
+		if (array[m] === target) {
+			return m;
+		} else if (array[m] > target) {
+			r = m - 1;
+		} else {
+			l = m + 1;
+		}
+	}
+	
+	return -1;
+};
+
+export default binarySearch;
