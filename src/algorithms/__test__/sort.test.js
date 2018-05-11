58b548f6b9864ff763d15726be4e293d65c53beb
Ziyue Chen <cokeeeeman@gmail.com>
2018-05-10 23:35:45 -0700

    Add mergeSort()

--- src/algorithms/__test__/sort.test.js
+++ src/algorithms/__test__/sort.test.js
@@ -1,6 +1,7 @@
 import selectionSort from '../selectionSort';
 import bubbleSort from '../bubbleSort';
 import insertionSort from '../insertionSort';
+import mergeSort from '../mergeSort';
 
 const testSorting = sorting => {
   describe(sorting.name, () => {
@@ -51,3 +52,4 @@ const testSorting = sorting => {
 testSorting(selectionSort);
 testSorting(bubbleSort);
 testSorting(insertionSort);
+testSorting(mergeSort);
