3f2428539fca8a306fb601e032c63c97eeaba759
Ziyue Chen <cokeeeeman@gmail.com>
2018-05-22 16:11:15 -0700

    Add comments for UndirectedGraph

--- src/data_structures/UndirectedGraph.js
+++ src/data_structures/UndirectedGraph.js
@@ -1,8 +1,18 @@
+/**
+ * Implementation of Undirected Graph using Adjacency List
+ * 1. Use Map to store all vertices;
+ * 2. For each key(vertice), the value is the neighbors in array
+ */
 class UndirectedGraph {
   constructor() {
     this.nodes = new Map();
   }
 
+  /**
+   * Add edge of node1 and node2
+   * @param {*} node1
+   * @param {*} node2
+   */
   addEdge(node1, node2) {
     if (this.nodes.has(node1)) {
       this.nodes.get(node1).push(node2);
@@ -17,6 +27,11 @@ class UndirectedGraph {
     }
   }
 
+  /**
+   * Get neighbors given a node
+   * @param  {*} node
+   * @return {Array} neighbors of input node
+   */
   getNeighbors(node) {
     if (this.nodes.has(node)) {
       return this.nodes.get(node);
@@ -25,6 +40,10 @@ class UndirectedGraph {
     }
   }
 
+  /**
+   * Get size of the graph
+   * @return {Number} size of the graph
+   */
   size() {
     return this.nodes.size;
   }
